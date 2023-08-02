a042fd79708dd220047abeae2581b774f3f2ee20
Dmitriy Vasilev <raoffonom@icloud.com>
2023-08-02 13:47:24 +0700

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
-};
+  extends: ['@react-native', 'plugin:prettier/recommended'],
+  env: {
+    'jest/globals': true,
+  },
+  rules: {
+    semi: ['off'],
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 2,
+    'react-native/no-raw-text': 2,
+    'react-native/no-single-element-style-arrays': 2,
+  },
+}
---
+++ src/components/Space/index.tsx
@@ -0,0 +1,17 @@
+import React, {memo} from 'react'
+
+import {View} from 'react-native'
+
+interface SpaceT {
+  height?: number | string
+  width?: number | string
+}
+
+const Space = memo<SpaceT>(({height, width}) => (
+  <View
+    testID="space-component"
+    style={{height: Number(height) || 0, width: Number(width) || 0}}
+  />
+))
+
+export {Space}
